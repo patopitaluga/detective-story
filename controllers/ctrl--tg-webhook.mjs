ffa6d41c6a7db06af95720587837fc39f5d7ef9b
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2023-05-25 12:51:42 -0300

    Added log

--- controllers/ctrl--tg-webhook.mjs
+++ controllers/ctrl--tg-webhook.mjs
@@ -8,7 +8,9 @@ const sendMessage = async(_chatId, _text) => {
   if (typeof _text !== 'string') throw new Error(`[sendMessage] _text argument was expected to be a string. Instead it\'s a ${typeof _text}`);
   if (_chatId === 'localtesting') return console.log(_text); // testing or using cli
 
-  return fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${_chatId}&text=${_text}&parse_mode=Markdown`)
+  const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${_chatId}&text=${_text}&parse_mode=Markdown`;
+  console.log(url);
+  return fetch(url)
     .catch((_err) => {
       console.log('Error sending message ' + _err.response.status + ' ' + _err.response.data);
     });
